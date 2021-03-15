f4c64228725cb70afc43ca7fb1c515c2d588ff3a
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-03-15 11:45:02 +0000

    added a bit more angular to the all_programmes page

--- static/all_programmes.js
+++ static/all_programmes.js
@@ -3,8 +3,18 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("programmeController", function($scope, $http){
+    document.getElementById("selected").style.display = "none";
 
     $http.get("/programmes").then(function(response){
         $scope.programmes = response.data
     });
+
+    // the below doesn't actually work as I haven't got the end point set up correctly.
+    $scope.selectProgramme = function(code){
+        $http.get("/programme/"+ code).then(function(response){
+            $scope.selectedProgramme = response.data
+            document.getElementById("selected").style.display = "block"
+        })
+    };
 });
+
