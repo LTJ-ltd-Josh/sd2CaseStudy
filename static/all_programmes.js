e45bdc48a7932e3de70460068a6ee2c12ba02765
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-03-15 11:21:57 +0000

    all programmes gets data from backend

--- static/all_programmes.js
+++ static/all_programmes.js
@@ -4,5 +4,7 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("programmeController", function($scope, $http){
 
-    $http.get("/programmes").then(function)
+    $http.get("/programmes").then(function(response){
+        $scope.programmes = response.data
+    });
 });
