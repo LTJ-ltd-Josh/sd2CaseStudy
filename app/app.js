047852690d77db8afa63d1b55087af1bb38efa76
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-03-22 10:32:36 +0000

    added delete module to front end

--- app/app.js
+++ app/app.js
@@ -78,3 +78,4 @@ app.listen(3000, function(err){
 });
 
 
+// can check recording for info about deleting.
--- static/all_modules.js
+++ static/all_modules.js
@@ -8,4 +8,18 @@ mainApp.controller("moduleController", function($scope, $http) {
   $http.get("/modules").then(function(response){
      $scope.modules = response.data 
   });
+
+  $scope.deleteModule = function(code){
+      // send delete message to node server
+      $http.delete("/module/" + code).then(function(response){
+        // refresh list of modules
+        $http.get("/modules").then(function(response){
+          $scope.modules = response.data;
+        });
+
+      });
+
+      
+
+  };
 });
